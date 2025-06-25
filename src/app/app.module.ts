063078260d5a54e82be580645c07dd522baf4547
Robert Alexandre <robert.alexandrea7@gmail.com>
2025-06-24 23:57:33 -0300

    Criados componentes de header e footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
 import { HomeComponent } from './home/home.component';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabecalhoComponent,
     RodapeComponent,
-    HomeComponent
+    HomeComponent,
+    CriarPensamentoComponent
   ],
   imports: [
     BrowserModule,
