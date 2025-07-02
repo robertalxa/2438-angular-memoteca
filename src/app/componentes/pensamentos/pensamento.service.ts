17540adc41124decf5a99bff2a8bfef14d9754f5
Robert Alexandre <robert.alexandrea7@gmail.com>
2025-07-01 23:31:49 -0300

    Trabalhando com o primeiro observer para retornar a lista de pensamentos do backend

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -2,6 +2,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Pensamento } from './pensamento';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root', // Isso daqui diz que o provedor desse serviço é o root, logo o serviço está disponível para toda a aplicação
@@ -10,7 +11,7 @@ export class PensamentoService {
   private readonly API = 'http://localhost:3000/pensamentos';
   constructor(private http: HttpClient) {}
 
-  listar() {
+  listar(): Observable<Pensamento[]> {
     return this.http.get<Pensamento[]>(this.API);
   }
 }
