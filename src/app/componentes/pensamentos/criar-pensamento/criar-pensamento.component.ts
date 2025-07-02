19e5fde54afa3fba5d88b9ee7c1e797163beb99c
Robert Alexandre <robert.alexandrea7@gmail.com>
2025-07-01 23:42:36 -0300

    Salvando pensamento criado no componente de criar

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -8,21 +10,23 @@ import { Pensamento } from '../pensamento';
 })
 export class CriarPensamentoComponent implements OnInit {
   pensamento: Pensamento = {
-    id: 1,
     conteudo: 'Aprendendo Angular',
     autoria: 'Dev',
     modelo: 'modelo1',
   };
 
-  constructor() {}
+  constructor(private service: PensamentoService, private router: Router) {}
 
   ngOnInit(): void {}
 
   criarPensamento() {
-    console.log('Salvando form');
+    this.service.criar(this.pensamento).subscribe((pensamento) => {
+      alert(`Pensamento de "${pensamento.autoria}" cadastrado!`);
+      this.router.navigate(['/listarPensamento']);
+    });
   }
 
   cancelarForm() {
-    console.log('Cancelando form');
+    this.router.navigate(['/listarPensamento']);
   }
 }
